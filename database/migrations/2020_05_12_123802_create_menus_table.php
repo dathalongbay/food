4fcf0c882aab5486042e42fbddabf00e7d028efc
dat <dathalongbay@gmail.com>
2020-05-12 19:40:54 +0700

    111

---
+++ database/migrations/2020_05_12_123802_create_menus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('food_name');
+            $table->text('food_intro');
+            $table->string('food_image');
+            $table->integer('food_price');
+            $table->integer('food_stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
