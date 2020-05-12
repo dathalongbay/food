148f7bb427d43bdd7f30a4114777140e3d06b17d
dat <dathalongbay@gmail.com>
2020-05-12 19:16:53 +0700

    111

--- resources/views/foodbackend/index.blade.php
+++ resources/views/foodbackend/index.blade.php
@@ -1,8 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 5/12/2020
- * Time: 6:57 PM
- */
-echo 'index';
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo '<br> chuỗi in bằng cú pháp php thuần';
+    $a = 'hà nội';
+    echo $a;
+    ?>
+    <h1>In ra biến bằng cú pháp của blade laravel</h1>
+    {{ $a }}
+
+    <br>
+    {{ 1 + 5 }}
+
+    {{ $b = 'đà nẵng' }}
+</body>
+</html>
