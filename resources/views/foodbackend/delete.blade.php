826385fba49d05ab27dc1b7d8540947f8fc88b88
dat <dathalongbay@gmail.com>
2020-05-12 19:28:01 +0700

    111

--- resources/views/foodbackend/delete.blade.php
+++ resources/views/foodbackend/delete.blade.php
@@ -1,8 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 5/12/2020
- * Time: 6:57 PM
- */
-echo 'delete';
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="container">
+    <h1>Xóa món ăn</h1>
+
+    <div class="row">
+        <form action="/action_page.php">
+            <div class="form-group">
+                <label for="email">Email address:</label>
+                <input type="email" class="form-control" placeholder="Enter email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" placeholder="Enter password" id="pwd">
+            </div>
+            <div class="form-group form-check">
+                <label class="form-check-label">
+                    <input class="form-check-input" type="checkbox"> Remember me
+                </label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/foodbackend/index.blade.php
+++ resources/views/foodbackend/index.blade.php
@@ -6,19 +6,45 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-    <?php
-    echo '<br> chuỗi in bằng cú pháp php thuần';
-    $a = 'hà nội';
-    echo $a;
-    ?>
-    <h1>In ra biến bằng cú pháp của blade laravel</h1>
-    {{ $a }}
 
-    <br>
-    {{ 1 + 5 }}
-
-    {{ $b = 'đà nẵng' }}
+<div class="container">
+    <h2>Quản lý sản phẩm</h2>
+    <p>The .table class adds basic styling (light padding and horizontal dividers) to a table:</p>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Tên</th>
+            <th>Hình ảnh</th>
+            <th>Giá</th>
+            <th>Hành động</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+        </tr>
+        <tr>
+            <td>Mary</td>
+            <td>Moe</td>
+            <td>mary@example.com</td>
+        </tr>
+        <tr>
+            <td>July</td>
+            <td>Dooley</td>
+            <td>july@example.com</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
